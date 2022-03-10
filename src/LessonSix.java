2007797d732e066de214b1bf9126dfbf87997ea3
KedBor <e.susha@bk.ru>
2022-03-10 16:39:45 +0300

    Initial commit

---
+++ src/LessonSix.java
@@ -0,0 +1,57 @@
+public class LessonSix {
+/*   //Переносит число в другое место и приравнивает a и b
+    public static void main (String[] args){
+        int b = testMethod();
+        System.out.println(b);
+    }
+    public static int testMethod(){
+        int a = 5;
+        return a;
+    }*/
+
+/*   //Переносит число в другое место
+    public static void main (String[] args){
+        System.out.println(testMethod());
+    }
+    public static int testMethod(){
+        int a = 5;
+        return a;
+    }*/
+
+/*   //Переносит Hello World в другое место
+    public static void main (String[] args){
+        testMethod1();
+    }
+    public static void testMethod1(){
+        System.out.println("Hello World");
+    }*/
+
+/*    // Возвращает сумму чисел и переносит их метод
+    public static void main (String[] args){
+        int sum = calculateSum(5, 6);
+        System.out.println(sum);
+    }
+    public static int calculateSum(int a, int b){
+        return a + b;
+        // Область видимости в фигурных скобках
+    }*/
+
+
+/*    public static void main (String[] args){
+        int[] array123 = new int[3];
+        int a = 15;
+        for (int i = 0; i < 3; i++){
+            System.out.println(array123[i]);
+        }
+        System.out.println(a);
+        updateArray(array123, a);
+        System.out.println(a);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(array123[i]);
+        }
+    }
+    public static void updateArray (int [] array, int b){
+        array[0] = 5;
+        b = 5;
+    }*/
+}
