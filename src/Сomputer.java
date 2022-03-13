b1da8c48c0323dd4543fc212a006891c47ec80ff
KydBor <e.susha@bk..ru>
2022-03-14 01:13:09 +0300

    Initial Commit

--- src/Сomputer.java
+++ src/Сomputer.java
@@ -1,3 +1,6 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Сomputer {
     String CPU;
     int RAM;
@@ -18,15 +21,21 @@ public class Сomputer {
         System.out.println("OnOff - " + OnOff);
     }
 
-    public static void OnOff(String[]args) {
-        Scanner scanner = new Scanner(System.in);
-        int [] array = new int [5];
-        for(int i = 0; i < 5; i++){
-            System.out.println("Введите число: ");
-            array[i] = scanner.nextInt();//тут выводим данные после run//
+    public static void main(String[] args) {
+        Random random = new Random(); //Создать экземпляр
+        int a = random.nextInt(2);//Создайте случайное число от 0 до 99 и назначьте его случайным образом
+        System.out.println(a);//  Показать напечатать это случайное число
+        //введите данные
+        Scanner scanner = new Scanner(System.in);//  Создать экземпляр ввода
+        System.out.println("Введите угаданный номер");
+        int i = scanner.nextInt();
+        if (a == i) {
+            System.out.println("Комп включается"); //Когда введенное число меньше случайного числа
+        } else if (a != i) {
+            System.out.println("Комп сорел");//  Когда введенное число больше случайного числа
         }
-        for(int i = 0; i < 5; i++){
-            System.out.println(array[i]);// эта команда выполняет данные после заполнения данных//
-        }
-            }
+
+    }
+
+
 }
